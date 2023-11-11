9db52bbb23578044ce13f0129943f41d7f225066
Joshua Hero <jhdcruz@proton.me>
2023-11-11 23:52:48 +0800

    add datetime formatter

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,37 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Merge classnames, primarily used only in shadcn/ui.
+ */
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
+/**
+ * Format date/timestamp into a human-readable date format.
+ */
 export function formatDate(input: string | number): string {
-    const date = new Date(input)
+  const date = new Date(input);
 
-    return date.toLocaleDateString("en-US", {
-        month: "long",
-        day: "numeric",
-        year: "numeric",
-    })
+  return date.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+}
+
+/**
+ * Format date/timestamp into a human-readable date & time format.
+ */
+export function formatDateTime(input: string | number): string {
+  const date = new Date(input);
+
+  return date.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  });
 }
