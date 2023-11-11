da0d17e6ff8002864320cb9ac668fcaded656061
Joshua Hero <jhdcruz@proton.me>
2023-11-12 00:28:29 +0800

    attach axiom logger on route handler

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,13 +1,15 @@
-import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import { type AxiomRequest, withAxiom } from "next-axiom";
 
-// Used for signing up users using email and password
-export async function GET(request: Request) {
+import { createClient } from "@/utils/supabase/server";
+
+// Used for SSR auth flows
+export const GET = withAxiom(async (req: AxiomRequest) => {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the Auth Helpers package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
-  const requestUrl = new URL(request.url);
+  const requestUrl = new URL(req.url);
   const code = requestUrl.searchParams.get("code");
 
   if (code) {
@@ -18,4 +20,4 @@ export async function GET(request: Request) {
 
   // URL to redirect to after sign in process completes
   return NextResponse.redirect(requestUrl.origin);
-}
+});
