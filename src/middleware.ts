7eb7215b82244dcc2c692b27b8310cde1fdb05b0
Joshua Hero <jhdcruz@proton.me>
2023-11-12 23:37:40 +0800

    fix infinite redirect to login in middleware auth check

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 import { createClient } from "@/utils/supabase/middleware";
 
@@ -12,7 +12,7 @@ export async function middleware(req: NextRequest) {
     error,
   } = await supabase.auth.getSession();
 
-  if (!session) {
+  if (!session && req.nextUrl.pathname.startsWith('/login')) {
     return NextResponse.redirect(new URL("/login", req.nextUrl).href);
   }
 
