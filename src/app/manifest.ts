7b578286e71f91fe3a07448b89f6ed2868c7ac10
Joshua Hero <jhdcruz@proton.me>
2023-11-12 23:19:34 +0800

    refactor manifest.json to manifest.ts

---
+++ src/app/manifest.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'SIA 305',
+    short_name: 'SIA 305',
+    description: 'SIA 305',
+    start_url: '/',
+    display: 'standalone',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  }
+}
