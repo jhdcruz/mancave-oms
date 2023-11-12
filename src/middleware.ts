bfe96824da627205051aede66f456446a87332ff
Joshua Hero <jhdcruz@proton.me>
2023-11-12 22:44:18 +0800

    move auth checks in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,25 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { type AxiomRequest } from "next-axiom";
+
 import { createClient } from "@/utils/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
-  try {
-    // This `try/catch` block is only here for the interactive tutorial.
-    // Feel free to remove once you have Supabase connected.
-    const { supabase, response } = createClient(request);
+export async function middleware(req: AxiomRequest) {
+  const { supabase, response } = createClient(req);
 
-    // Refresh session if expired - required for Server Components
-    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-    await supabase.auth.getSession();
+  // Refresh session if expired - required for Server Components
+  // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
 
-    return response;
-  } catch (e) {
-    // If you are here, a Supabase client could not be created!
-    // This is likely because you have not set up environment variables.
-    // Check out http://localhost:3000 for Next Steps.
-    return NextResponse.next({
-      request: {
-        headers: request.headers,
-      },
+  if (!session) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl).href, {
+      status: 401,
     });
   }
+
+  // TODO: Protected routes
+
+  return response;
 }
