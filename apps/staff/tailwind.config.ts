37440f60c3346ec8060cb791acb8c9465a65b750
Joshua Hero <jhdcruz@proton.me>
2023-11-17 21:23:01 +0800

    merge staff and admin portal

--- apps/staff/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "tailwindcss";
-import defaultConfig from "@mcsph/tooling/tailwind/tailwind.config";
-
-const config: Pick<Config, "presets"> = {
-  presets: [defaultConfig],
-};
-
-export default config;
