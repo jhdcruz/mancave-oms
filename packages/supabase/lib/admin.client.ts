c2c7bb4051e331a62be98827d70aad7a5f0c7b19
Joshua Hero <jhdcruz@proton.me>
2023-11-18 22:37:33 +0800

    add admin service level on supabase client

---
+++ packages/supabase/lib/admin.client.ts
@@ -0,0 +1,18 @@
+import { createBrowserClient } from '@supabase/ssr';
+
+/**
+ * Supabase client for client-side with elevated permission
+ *
+ * Any method under the supabase.auth.admin namespace
+ * requires a service_role key.
+ *
+ * These methods are considered admin methods and should
+ * be called on a trusted server.
+ *
+ *  Never expose your `service_role` key in the browser.
+ */
+export const adminBrowserClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ADMIN_KEY!,
+  );
---
+++ packages/supabase/lib/admin.server.ts
@@ -0,0 +1,50 @@
+import { type CookieOptions, createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+import { Logger } from 'next-axiom';
+
+/**
+ * Supabase client for server with elevated permission
+ *
+ * Any method under the supabase.auth.admin namespace
+ * requires a service_role key.
+ *
+ * These methods are considered admin methods and should
+ * be called on a trusted server.
+ *
+ *  Never expose your `service_role` key in the browser.
+ */
+export const adminServerClient = (cookieStore: ReturnType<typeof cookies>) => {
+  const log = new Logger();
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ADMIN_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value, ...options });
+          } catch (error) {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+            log.error('Error setting supabase server client cookie', { error });
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value: '', ...options });
+          } catch (error) {
+            // The `delete` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+            log.error('Error supabase server client cookie', { error });
+          }
+        },
+      },
+    },
+  );
+};
