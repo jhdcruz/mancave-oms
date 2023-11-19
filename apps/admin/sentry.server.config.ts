8cb077a8cdf6e3f8b27a0d0e8e92334987961926
Joshua Hero <jhdcruz@proton.me>
2023-11-20 02:45:40 +0800

    admin: add sentry profiles sample rate

--- apps/admin/sentry.server.config.ts
+++ apps/admin/sentry.server.config.ts
@@ -4,6 +4,7 @@ import { ProfilingIntegration } from "@sentry/profiling-node";
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 0.5,
+  profilesSampleRate: 0.5,
   debug: false,
   integrations: [
     new ProfilingIntegration(),
