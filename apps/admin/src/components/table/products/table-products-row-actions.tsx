ae2eac208dbbfe2ba4654736dd572dca84961f95
jhdcruz <jhdcruz@proton.me>
2023-11-28 19:26:52 +0800

    style(admin): remove redundant consts

--- apps/admin/src/components/table/products/table-products-row-actions.tsx
+++ apps/admin/src/components/table/products/table-products-row-actions.tsx
@@ -41,7 +41,7 @@ export function TableProductsRowActions<TData>({
 
     const supabase = browserClient();
 
-    const { error } = await deleteProduct(id, { supabase: supabase });
+    await deleteProduct(id, { supabase: supabase });
 
     setDeleteDialog(false);
     setDeleteLoading(false);
@@ -57,7 +57,7 @@ export function TableProductsRowActions<TData>({
 
     if (!data.session) {
       log.info('Session data not found');
-      log.flush();
+      await log.flush();
       return redirect('/login', RedirectType.replace);
     }
 
@@ -78,7 +78,7 @@ export function TableProductsRowActions<TData>({
         <DropdownMenuTrigger asChild>
           <Button
             variant="ghost"
-            className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+            className="flex p-0 data-[state=open]:bg-muted"
           >
             <MoreHorizontal className="h-4 w-4" />
             <span className="sr-only">Open menu</span>
