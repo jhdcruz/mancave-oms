fbe75ac77d23b3f9837d5fe4725344bda55866c6
jhdcruz <jhdcruz@proton.me>
2023-11-30 15:48:46 +0800

    fix(utils): append url protocols depends on env

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -8,18 +8,23 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-const url = () => {
-  return (
-    process.env.NEXT_PUBLIC_SITE_URL ||
-    process.env.VERCEL_URL ||
-    'http://localhost:3000'
-  );
+const getUrl = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ??
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ??
+    'http://localhost:3000' ??
+    'http://localhost:3001';
+
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}/`;
+
+  return url;
 };
 
 /**
  * Determines the default URL for the application.
  */
 // don't ask.
-export const defaultUrl = url();
+export const defaultUrl = getUrl();
 
 export { formatDate, formatDateTime } from './lib/date';
