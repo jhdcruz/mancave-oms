342129cff743e61579a81719bc94ffd72552682f
jhdcruz <jhdcruz@proton.me>
2023-12-05 14:08:47 +0800

    fix(admin): redirect `/` to inventory temporarily

--- apps/admin/src/app/(auth)/page.tsx
+++ apps/admin/src/app/(auth)/page.tsx
@@ -1,9 +1,13 @@
 import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Dashboard | Man Cave Supplies PH, Inc.',
 };
 
 export default function Index() {
+
+  redirect('/inventory');
+
   return <>Wassup</>;
 }
